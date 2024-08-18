1f030bc7e9020803b66906878157b3a885f2eeb4
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-08-18 17:59:53 +0300

    fin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import openMenu from './js/openMenu';
+import accordionFaq from './js/faq';
 import { toggleReviews } from './js/reviews';
 import { submitWorkTogether } from './js/workTogether';
 import './js/about';
 
 // header
 openMenu();
+
+// faq
+accordionFaq;
 
 //  section Reviews
 toggleReviews();
