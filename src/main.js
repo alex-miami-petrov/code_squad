ce688290c56cb689151ef0d0398de9722ce696da
Miami <alex.miami.petrov@gmail.com>
2024-08-18 12:48:32 +0300

    m

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import openMenu from './js/openMenu';
 import { toggleReviews } from './js/reviews';
 import { submitWorkTogether } from './js/workTogether';
+import './js/about';
+
+// header
 openMenu();
 
 //  section Reviews
@@ -8,5 +11,3 @@ toggleReviews();
 
 // work-together
 submitWorkTogether();
-
-import './js/about';
