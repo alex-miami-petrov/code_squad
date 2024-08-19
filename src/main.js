957e94523a130c98bfdb6a4ee07d8e717c037fa5
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-08-19 09:35:44 +0200

    added section covers
    Section covers for phone, tablet and desktop. Added animation , js - viewpoint and html - ul,li,img.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import openMenu from './js/openMenu';
 import { toggleReviews } from './js/reviews';
+import initCoversAnimation from './js/covers';
 
 openMenu();
 
 //  section Reviews
 toggleReviews();
+
+// section covers 
+document.addEventListener('DOMContentLoaded', () => {
+    initCoversAnimation();  // Викликаємо анімацію covers
+});
